8f27049ef1bf6167a549500db13c0e28366afa92
24parida <68526448+24parida@users.noreply.github.com>
2024-05-27 14:03:31 -0500

    Time: 177 ms (5.05%) | Memory: 13.4 MB (17.64%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        minL = [0] * len(height)
+        minR = [0] * len(height)
+        res = 0
+
+        for x, i in enumerate(height):
+            if x==0: minL[0] = i
+            else: minL[x] = max(minL[x-1], i)
+
+        for i in range(len(height)-1,-1,-1):
+            if i==(len(height)-1): minR[len(height)-1] = height[i]
+            else: minR[i] = max(minR[i+1], height[i])
+
+        temp = [0] * len(height)
+        for i in range(1, len(height) - 1):
+            res += max(min(minL[i-1], minR[i+1]) - height[i], 0)
+            temp[i] = min(minL[i-1], minR[i+1]) - height[i]
+
+        print(minL)
+        print(minR)
+        print(temp)
+        return res
