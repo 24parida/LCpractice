03ebd1cfcc1122c44aa5bb6a82303d02c64029ba
24parida <68526448+24parida@users.noreply.github.com>
2025-09-03 16:06:23 -0500

    Time: 151 ms (46.70%) | Memory: 33.8 MB (87.68%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,31 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        anc_map = [[] for _ in range(n)]
+        res = []
+
+        for top, bot in edges:
+            anc_map[bot].append(top)
+
+        print(anc_map)
+        for i in range(n):
+            temp_res = []
+            neighbors = anc_map[i][:]
+            visited = set()
+            while neighbors:
+                if i == 5: print(neighbors)
+                    
+                neigh = neighbors.pop()
+                if neigh in visited: 
+                    continue
+            
+                neighbors.extend(anc_map[neigh])
+                temp_res.append(neigh)
+                visited.add(neigh)
+            
+            temp_res = sorted(temp_res)
+            res.append(temp_res)
+        
+        return res
+        
+        
+
