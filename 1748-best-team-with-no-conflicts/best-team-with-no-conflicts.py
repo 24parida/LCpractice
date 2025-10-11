f3849178a5aa937395e62a68a347aebe5dbb3a86
24parida <68526448+24parida@users.noreply.github.com>
2025-10-11 18:05:27 -0500

    Time: 883 ms (69.61%) | Memory: 18.1 MB (62.34%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.py
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.py
@@ -3,11 +3,11 @@ class Solution:
         combined = [i for i in zip(ages, scores)]
         combined.sort()
 
-        dp = [0 for _ in combined]
+        dp = [_[1] for _ in combined]
 
         for i in range(len(combined)):
             age_i, score_i  = combined[i]
-            dp[i] = score_i
+            # dp[i] = score_i
             for j in range(i):
                 _, score_j, = combined[j]
                 if score_j <= score_i:
