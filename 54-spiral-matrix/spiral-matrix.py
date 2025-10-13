e156b909b0c2ca2f4ba8705f9cfb125b0bcf0135
24parida <68526448+24parida@users.noreply.github.com>
2025-10-13 16:33:04 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (18.46%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,19 +1,32 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        size = len(matrix) * len(matrix[0])
+        n, m = len(matrix), len(matrix[0])
+        i = j = 0
+
+        ## right, down, left, up
+        cur = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         res = []
-        while matrix and len(res) != size:
-            if matrix and len(res) != size:
-                temp = matrix.pop(0)
-                res.extend(temp)
-            if matrix and len(res) != size:
-                for row in matrix:
-                    res.append(row.pop())
-            if matrix and len(res) != size:
-                temp = matrix.pop()
-                temp = temp[::-1]
-                res.extend(temp)
-            if matrix and len(res) != size:
-                for row in matrix[::-1]:
-                    res.append(row.pop(0))
+        visited = set()
+
+        def isValid(x, y):
+            return (x,y) not in visited and x < n and x >= 0 and y < m and y >= 0
+        
+
+        while isValid(i, j) and len(res) < n * m:
+            res.append(matrix[i][j])
+            visited.add((i, j))
+
+            dirx, diry = directions[cur]
+            nxt_i, nxt_j = i + dirx, j + diry
+
+            if isValid(nxt_i, nxt_j):
+                i, j = nxt_i, nxt_j
+            else:
+                cur = (cur + 1) % 4
+                dirx, diry = directions[cur]
+                nxt_i, nxt_j = i + dirx, j + diry
+                i, j = nxt_i, nxt_j
+        
         return res
+        
