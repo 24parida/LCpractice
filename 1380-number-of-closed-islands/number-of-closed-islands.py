da694881324737649571f5b384c8d5096ab732b6
24parida <68526448+24parida@users.noreply.github.com>
2025-10-18 16:06:48 -0500

    Time: 111 ms (5.36%) | Memory: 19.5 MB (15.03%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        res = 0
+
+        directions = [(-1, 0), (1, 0), (0, 1), (0,-1)]
+        visited = set()
+
+        N = len(grid)
+        M = len(grid[0])
+        is_in_bounds = lambda x,y: (x >= 0 and x < N) and (y >= 0 and y < M)
+
+        def bfs(nodeX, nodeY):
+            q = collections.deque([(nodeX, nodeY)])
+            touches_border = False
+            while q:
+                x,y = q.popleft()
+                if x == N-1 or y == M-1 or x == 0 or y == 0:
+                    touches_border = True
+                visited.add((x, y))
+                for nx, ny in directions:
+                    new_x, new_y = x+nx, y+ny
+                    if is_in_bounds(new_x, new_y) and (new_x, new_y) not in visited and grid[new_x][new_y] == 0:
+                        q.append((new_x, new_y))
+            
+            return not touches_border
+        
+        for i in range(N):
+            for j in range(M):
+                if (i,j) not in visited and grid[i][j] == 0:
+                    res += 1 if bfs(i,j) else 0
+        
+        return res
+
+                
