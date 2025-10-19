555a557f086215a4e466281606f34f78fba36cbe
24parida <68526448+24parida@users.noreply.github.com>
2025-10-19 13:41:36 -0500

    Time: 495 ms (38.75%) | Memory: 18.3 MB (36.02%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.py
@@ -0,0 +1,46 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        res, sol = [], []
+        
+        def backtrack(i, cur, prev):
+            if i >= len(num):
+                if cur == target:
+                    res.append("".join(sol))
+                return
+
+            for j in range(i, len(num)):
+                parsed_num = int(num[i:j+1])
+
+                if i == 0:
+                    sol.append(str(parsed_num))
+                    backtrack(j+1, parsed_num, parsed_num)
+                    sol.pop()
+                    if num[i] == '0':
+                        break
+                    continue
+
+                # try add
+                sol.append(f"+{parsed_num}")
+                backtrack(j+1, cur + parsed_num, parsed_num)
+                sol.pop()
+
+                # try sub
+                sol.append(f"-{parsed_num}")
+                backtrack(j+1, cur - parsed_num, -parsed_num)
+                sol.pop()
+
+                # try mult
+                sol.append(f"*{parsed_num}")
+                new_num = (cur - prev) + (prev * parsed_num)
+                new_prev = prev * parsed_num
+                backtrack(j+1, new_num, new_prev)
+                sol.pop()
+
+                if num[i] == '0':
+                    break
+
+        backtrack(0, 0, 0)
+        return res
+
+            
+            
