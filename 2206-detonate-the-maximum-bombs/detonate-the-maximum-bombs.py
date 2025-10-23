d5dbc94e413493bd4a1e61d34a253f56096e6271
24parida <68526448+24parida@users.noreply.github.com>
2025-10-23 15:50:36 -0500

    Time: 943 ms (14.25%) | Memory: 20.7 MB (5.07%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        graph = collections.defaultdict(list)
+        for i in range(len(bombs)):
+            for j in range(i+1, len(bombs)):
+                x, y, r1 = bombs[i]
+                x2, y2, r2 = bombs[j]
+
+                dist = lambda x, y, i, j: sqrt((x-i)**2 + (y-j)**2)
+                distance = dist(x,y,x2,y2)
+
+                if distance <= r1:
+                    graph[(i, x, y)].append((j, x2, y2))
+                if distance <= r2:
+                    graph[(j, x2, y2)].append((i, x, y))
+        
+
+        res = 1
+        for bomb in list(graph.keys()):
+            visited = set()
+            def dfs(node):
+                if node in visited:
+                    return 0
+                visited.add(node)
+                largest_neighbor = 0
+                for neigh in graph[node]: largest_neighbor += dfs(neigh)
+                return 1 + largest_neighbor
+
+            local_res = dfs(bomb)
+            res = max(local_res, res)
+        
+        return res
