93811f76cdc6f4f3eff38cd89b39ee19222ab9e4
24parida <68526448+24parida@users.noreply.github.com>
2025-10-26 18:21:04 -0500

    Time: 65 ms (75.21%) | Memory: 18.1 MB (54.37%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,19 +1,22 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        c1 = collections.Counter(s)
-        c2 = collections.Counter(t)
+        # c1 = collections.Counter(s)
+        # c2 = collections.Counter(t)
 
-        diffs = 0
+        # diffs = 0
         
-        for elem in c1:
-            if elem in c2:
-                diffs += abs(c1[elem] - c2[elem])
-            else:
-                diffs += c1[elem]
+        # for elem in c1:
+        #     if elem in c2:
+        #         diffs += abs(c1[elem] - c2[elem])
+        #     else:
+        #         diffs += c1[elem]
         
-        for elem in c2:
-            if elem not in c1:
-                diffs += c2[elem]
+        # for elem in c2:
+        #     if elem not in c1:
+        #         diffs += c2[elem]
         
-        return diffs // 2
+        # return diffs // 2
+
+        c1, c2 = Counter(s), Counter(t)
+        return sum((c1 - c2).values())
         
