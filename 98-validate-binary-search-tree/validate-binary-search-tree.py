57bdf9d888c158ff02b9386e2aa287253681289a
24parida <68526448+24parida@users.noreply.github.com>
2025-10-26 17:48:59 -0500

    Time: 6 ms (5.45%) | Memory: 19.7 MB (68.39%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return (True, float("inf"), float("-inf"))
+            if not (node.left or node.right):
+                return (True, node.val, node.val) # isBST, minRight, maxLeft
+            
+            isValidRight, minRight, maxRight = dfs(node.right)
+            isValidLeft, minLeft, maxLeft = dfs(node.left)
+            
+            isValidNode = (isValidRight and isValidLeft) and (maxLeft < node.val < minRight)
+            return (isValidNode, min(node.val, minLeft), max(node.val, maxRight))
+        
+        return dfs(root)[0]
+
+        
