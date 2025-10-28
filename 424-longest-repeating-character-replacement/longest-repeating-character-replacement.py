4f2d171895134ea04b54d0c1ff15875cb699df3d
24parida <68526448+24parida@users.noreply.github.com>
2025-10-28 16:39:27 -0500

    Time: 75 ms (98.44%) | Memory: 17.9 MB (86.78%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,24 +1,16 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        cnt = [0] * 26
-        res, l = 0, 0
-        curMax = -1
-
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counts = collections.defaultdict(int)
+        res = 0
+        l = 0
+        maxF = 0
         for r in range(len(s)):
-            cnt[ord(s[r].lower()) - ord('a')] += 1
-            curMax = max(cnt[ord(s[r].lower()) - ord('a')], curMax)
-
-            while (r-l+1 - curMax) > k:
-                cnt[ord(s[l].lower()) - ord('a')] -= 1
+            counts[s[r]] += 1
+            maxF = max(counts[s[r]], maxF)
+            while r - l + 1 - maxF > k:
+                counts[s[l]] -= 1
                 l += 1
 
-            res = max(res, r-l+1)
-        
+        res = max(r-l+1, res)
+
         return res
-            
-        
