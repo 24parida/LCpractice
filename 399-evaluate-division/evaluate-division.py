154e54596016f75ab18d6f279c04a2626e805983
24parida <68526448+24parida@users.noreply.github.com>
2025-10-28 21:20:38 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (47.95%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,48 +1,31 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        self.graph = collections.defaultdict(list)
 
-        adj_list = {}
         for i in range(len(equations)):
-            l1, l2 = equations[i]
+            a, b = equations[i]
             value = values[i]
+            self.graph[a].append((b, value))
+            self.graph[b].append((a, 1/value))
 
-            if l1 not in adj_list:
-                adj_list[l1] = [(l2, value)]
-            else:
-                adj_list[l1].append((l2, value))
-            
-            if l2 not in adj_list:
-                adj_list[l2] = [(l1, 1/value)]
-            else:
-                adj_list[l2].append((l1, 1/value))
-        
-        res = [-1.0] * len(queries)
+        res = []
+        for a, b in queries:
+            self.visited = set()
+            res.append(self.dfs(a, b))
 
-        for i in range(len(queries)):
-            num, div = queries[i]
-            if num not in adj_list:
-                continue
-
-            stack = [num]
-            visited = set()
-
-            def look_up(running_value, curr_node, look_up_node):
-                if curr_node in visited:
-                    return 0
-
-                visited.add(curr_node)
-                for neigh, val in adj_list[curr_node]:
-                    if neigh == look_up_node:
-                        return running_value * val
-                    temp_res = look_up(running_value * val, neigh, look_up_node)
-                    if temp_res != 0:
-                        return temp_res
-                return 0
-            
-            temp = look_up(1.0, num, div)
-            if temp != 0:
-                res[i] = temp
         return res
-                    
 
+    def dfs(self, a, b):
+        if a in self.graph and a == b:
+            return 1.0
+        if (a not in self.graph or b not in self.graph) or a in self.visited:
+            return -1.0
 
+        self.visited.add(a)
+        
+        for n, weight in self.graph[a]:
+            res = self.dfs(n, b)
+            if res != -1.0:
+                return weight * res
+        
+        return -1.0
